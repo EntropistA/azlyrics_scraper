9b1a8845ba775cebba1b7c7e87333f694b39a356
Adam <adam.wilcz7@gmail.com>
2022-05-26 14:50:41 +0200

    Get all albums and songs from an artist

--- azlyrics_scraper.py
+++ azlyrics_scraper.py
@@ -13,25 +13,64 @@ def get_soup(url: str) -> BeautifulSoup:
     return BeautifulSoup(page, features="html.parser")
 
 
-ArtistAndLink = namedtuple("ArtistAndLink", "artist link")
+Artist = namedtuple("Artist", "name link")
+Album = namedtuple("Album", "name songs")
+Song = namedtuple("Song", "name link")
 
 
 def artists_by_letter(letter: str) -> list:
-    soup = get_soup(f"{BASE_URL}/{letter.lower()}.html")
-    artists_and_links = []
+    url = f"{BASE_URL}/{letter.lower()}.html"
+    soup = get_soup(url)
+    names_and_links = []
     for div in soup.find_all("div", {"class": "container main-page"}):
         links = div.findAll("a")
         for a in links:
-            artists_and_links.append(
-                ArtistAndLink(a.text.strip(), a["href"])
+            names_and_links.append(
+                Artist(a.text.strip(), a["href"])
             )
-    return artists_and_links
+    return names_and_links
 
 
 def artists_names_by_letter(letter: str) -> list:
-    return [artist_and_link.artist for artist_and_link in artists_by_letter(letter)]
+    return [artist.name for artist in artists_by_letter(letter)]
 
 
 def artists_links_by_letter(letter: str) -> list:
-    return [artist_and_link.link for artist_and_link in artists_by_letter(letter)]
+    return [artist.link for artist in artists_by_letter(letter)]
 
+
+def albums_and_songs(artist_link: str) -> list:
+    url = f"{BASE_URL}/{artist_link}"
+    soup = get_soup(url)
+    albums_names_and_songs = []
+    current_album_name = ""
+    current_songs = []
+
+    albums_div = soup.find("div", id="listAlbum")
+    for div in albums_div.find_all("div"):
+        class_ = div.get("class")
+        if not class_:
+            continue
+        if "album" in class_:
+            if current_album_name:
+                albums_names_and_songs.append(
+                    Album(current_album_name, current_songs)
+                )
+                current_songs = []
+            current_album_name = div.find("b").text.strip('"')
+        elif "listalbum-item" in class_:
+            a = div.find("a")
+            current_songs.append(
+                Song(a.text, a["href"])
+            )
+    albums_names_and_songs.append(
+        Album(current_album_name, current_songs)
+    )
+    return albums_names_and_songs
+
+
+# al = artists_links_by_letter("T")
+# print(al[0])
+# print(albums_and_songs(al[0]))
+# print(dict(albums_and_songs(al[0])))
+# print(dict(albums_and_songs(al[0])[0]))
