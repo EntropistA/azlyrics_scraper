21a24295c7fc31a7cc657f2994f22e009627e61a
Adam <adam.wilcz7@gmail.com>
2022-05-27 12:15:35 +0200

    search function improvement

--- azlyrics_scraper.py
+++ azlyrics_scraper.py
@@ -1,11 +1,15 @@
+import string
 from collections import namedtuple
 
+import bs4
 from bs4 import BeautifulSoup
 import requests
 
+# TODO: mention
 # https://github.com/adhorrig/azlyrics
 
 BASE_URL = "https://www.azlyrics.com"
+SEARCH_URL = "https://search.azlyrics.com"
 
 
 def get_soup(url: str) -> BeautifulSoup:
@@ -16,6 +20,7 @@ def get_soup(url: str) -> BeautifulSoup:
 Artist = namedtuple("Artist", "name link")
 Album = namedtuple("Album", "name songs")
 Song = namedtuple("Song", "name link")
+Search = namedtuple("Search", "songs_results artist_results albums_results lyrics_results")
 
 
 def artists_by_letter(letter: str) -> list:
@@ -42,6 +47,7 @@ def artists_links_by_letter(letter: str) -> list:
 def albums_and_songs(artist_link: str) -> list:
     url = f"{BASE_URL}/{artist_link}"
     soup = get_soup(url)
+
     albums_names_and_songs = []
     current_album_name = ""
     current_songs = []
@@ -69,8 +75,64 @@ def albums_and_songs(artist_link: str) -> list:
     return albums_names_and_songs
 
 
-# al = artists_links_by_letter("T")
-# print(al[0])
-# print(albums_and_songs(al[0]))
-# print(dict(albums_and_songs(al[0])))
-# print(dict(albums_and_songs(al[0])[0]))
+def lyrics(song_link: str) -> str:
+    url = f"{BASE_URL}/{song_link}"
+    soup = get_soup(url)
+
+    lyrics_div = soup.find("div", id=None, class_=None)
+    return lyrics_div.text.strip()
+
+
+# def text_without_punctuation_marks(text: str) -> str:
+#     for punctuation_mark in '!"+,-.:?„':
+#         text = text.replace(punctuation_mark, "")
+#     return text
+
+
+def text_without_numbering(text: str) -> str:
+    return text.strip(string.digits + '. ')
+
+
+def _song_lyrics_name(name: str):
+    return name.split(" - ")[0].strip('"')
+
+
+def search(term: str) -> Search:
+    url = f"{SEARCH_URL}/search.php?q={term}"
+    soup = get_soup(url)
+
+    panels = soup.find_all("div", class_="panel")
+    songs_results, artists_results, albums_results, lyrics_results = [], [], [], []
+
+    for panel in panels:
+        for a in panel.find_all("a", class_=None):
+            name = text_without_numbering(a.text)
+            if "Song results" in panel.text:
+                name = _song_lyrics_name(name)
+                songs_results.append(
+                    Song(name, a["href"])
+                )
+            if "Artist results" in panel.text:
+                artists_results.append(
+                    Artist(name, a["href"])
+                )
+            if "Album results" in panel.text:
+                albums_results.append(
+                    Album(name, a["href"])
+                )
+            if "Lyrics results" in panel.text:
+                name = _song_lyrics_name(name)
+                lyrics_results.append(
+                    Song(name, a["href"])
+                )
+
+    return Search(
+        songs_results,
+        artists_results,
+        albums_results,
+        lyrics_results,
+    )
+
+
+
+print(search("Jarmark"))
